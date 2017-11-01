5eae71d5e196c798cd3f1b3ad8a43ca07d69fe28
dickanirwansyah <dickanirwansyah@gmail.com>
2017-11-01 20:29:23 +0700

    source java

---
+++ src/test/java/com/dickanirwansyah/jwt/springbootjwtauthentication/SpringbootJwtAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dickanirwansyah.jwt.springbootjwtauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootJwtAuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
